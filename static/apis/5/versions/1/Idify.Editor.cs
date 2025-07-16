c5c1c9b8febae9d40fb3cee51fd844d97a8700b9
BigTylis <125610445+BigTylis@users.noreply.github.com>
2025-07-16 19:44:00 -0400

    Add files via upload

---
+++ static/apis/5/versions/1/Idify.Editor.cs
@@ -0,0 +1,132 @@
+#if UNITY_EDITOR
+
+// Imports
+using UnityEngine;
+using UnityEditor;
+using System;
+
+/// <summary>
+/// Desc: An API tool for giving GameObjects unique and constant identification, then retrieving them through fast simple lookups.
+/// Usage: Simply add the BetterIdentifier component to GameObjects you would like to register. During runtime, retrieve them by their IDs using Stellar.APIs.ObjectHandlingExtensions.ObjectHandlingExtensions
+/// 
+///                ______
+///            .- '      `-.           
+///          .'            `.         
+///         /                \        
+///        ;                  ;`       
+///        |                  |;
+///        ;                 ;|
+///        '\               / ;       
+///         \`.           .' /        
+///          `.`-._____.- ' .'
+///            / /`_____.- '           
+///           / / /
+///          / / /
+///         / / /
+///        / / /
+///       / / /
+///      / / /
+///     / / /
+///    / / /
+///   / / /
+///   \/_/
+///   
+/// Credit: Written and Documented entirely by BigTylis
+/// </summary>
+namespace Stellar.APIs.Idify.Internal
+{
+    /// <summary>
+    /// Modifies the inspector GUI so that custom component properties appear correctly.
+    /// </summary>
+    [CustomEditor(typeof(BetterIdentifier))]
+    public class BetterIdentifierInspectorGUI : Editor
+    {
+        private string BetterIdentifierComponentIcon = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAABcGlDQ1BpY2MAACiRdZG7S8NQFMa/PkTRSkEdRBwyVOnQYlEQR6lgl+rQVrDqktwmrZCk4SZFiqvg4lBwEF18Df4HugquCoKgCCKurr4WKfHcptAi7Qk358d3z3e491zAn9aZYQcTgGE6PJNKSqv5Nan3HT4EMYQpRGVmW0vZxRy6xs8jVVM8xEWv7nUdY6Cg2gzw9RHPMos7xPPE6S3HErxHPMJKcoH4hDjG6YDEt0JXPH4TXPT4SzDPZRYAv+gpFdtYaWNW4gZxlDhi6BXWPI+4SUg1V7KUx2iNw0YGKSQhQUEFm9DhIE7ZpJl19iUavmWUycPob6EKTo4iSuSNkVqhripljXSVPh1VMff/87S1mWmveygJ9Ly67ucE0LsP1Guu+3vquvUzIPACXJstf5nmNPdNeq2lRY6B8A5wedPSlAPgahcYfbZkLjekAC2/pgEfF8BgHhi+B/rXvVk193H+BOS26YnugMMjYJLqwxt/HahoGNnG3eUAAAAJcEhZcwAALiMAAC4jAXilP3YAAAa0SURBVHja7Vt9UFRVFD+P3UX5VPkcSm38AjQpUEcHHB0mi2m0Mh3TUigcB0ytGYgsNaiBdGKaLLEwzf5gEPvDGKEPYEbTikaT0aGyUoaEiCwQ/EhYFBZ2X+cu6y4s9769+/bt8hDPzOG9Pe/u3ff7nXPvPfe8hwB3uxwU75yJdlcE8scLRoeIFFvwaCFAZNh7rGEwjKJDHYd61Q0hLwXeit1TBHSgBsjwnMYBCJfAu3MI+Flu4I4GyOiD3KQJBEGEjUdEGBcxxwnwJh7wkCYoHgGLUKsV621xOkDKAdvnK/UA2VHFePaCBPguVF8e8EpGgK/F09WK0jkQPJHwSIBtp5+3/NbjFPBnucAPECUIEC2syxED6i2nvjEtHmBLOTmrshvnOajzuMCnCdKsODVG+eRa6U4x2JnOV2UL38HkuETIqaU3OP4+wJGs/vPEzVtgXWEho6tQ6yqTJvCFBYe8i7rVQZvrCDrI1fBaFQ7JeCihXvwqF+CbAoCC66yvxyPoM4686Kz0omolrpsQuKKrC5KQRka7k197B8Hv4AljxcAjcLclEkjCHjxkcDb/HcHP5mnojKf2SYDvdSd4M7lXzOAPOWzY10PGejRvv7wEkIlkE+Paiwhe54l0EklIwcNJ9nqETtg0FiwZ5CGePnmHgMgI+XYLOR4VHA6/4iFmyIWhs3w4apurEcCK7aXDAd4SCQT8wGWvj7bEkdzR1Qh4CPUXir0dvT8s4CXyhifJwki51IoaIZcAkXe27/a2JXRjeseCwFgJB7ajicaoBZ3RWy4JLQywghwCDqOu5V3qntuWAL0ag/n8wIeVENwRRr/JN+Y5BEIInKAPhUntU2DGv7MhtiEBprRGAc/eDUlgOdLL2TlgLS30PRHOPbpuaJ3wN5yNrIbPEvfBaxuSISv9WfhhdhWIgnT2nbr0g2RnHM1a1+czQl/RcT+reQ5oTNpBjiLg//O/Bu2BrYPANoc2QMHyHDgeVwYZ5bsgqJN+K08kZJQUVWbSLnXS6hIsAmpoJQey0ChJwOuf7wa/7gBmFNTffx5+nHkCvo+pMH8mcmFyLexITYW84oMQdvM+ZkZKGQr+LmWC2GkaeFDIPBDTNB/Sq7ZDYeGXEH9xifXa1cArkL8mE3q1Bpd/h0aAP6hMxncFwStH8+GpMymDhkRZQpHU12iTxTkeAjootk+GmwSyAqScfBke/nOBbTe8oARuj+liRKzpGsU8l4cAgRL+6WqIBJJbrD+WZf18G3OKmqhvWa1D5Q4BVcvEq1Phwb9sjqydfkrxOUD1Etew0HreEHFh9BEwtdW23W8b1wJGL6NUAcdevEc8ASEd4bbUCZMlvc9NVtMyim2lFAG0woZBbQSMNfgMvkFtD7XdisXbSinmXVIE0EJGpzYC7EPeS9TQ3V+dv8jRks4zBAS1EaD3GZyq+N8OZDWlZa/7R/wcQHaK1ixRH2xOm1mjhWK7OeIJqJtoK1JNa5k1upZBE876NdG2wnBcY7z7CcCt5Uq1EHBq1jHzbtA8Oxu9YeGFJMUJoL2IcFQN4Dt8b0Dxo3usnx+rXQEBt8aznNZNMV/nIeAnNYY+2fXlr86CG/79D3pJRWhN9UbJdIFiC5Y9B3x9ek/ycIFvCq+H7anrzRUiIlqjzlwW8+sOdEtBhMhme0NRZWaJp5MdUv4qeDobtm5IhsshjVbwWUfzzfVEiTmLZu6jGVk1wY+h/2GofceHS3eK65QCWbykAHR9tr2JCUGTUG/HDU5T2B9DniGQGmBG2S6I/CeG2acomurwEM2b0Uo95yeDLcTOtk5JL5+ILedq59PjB0vPrYEVp1NxH+Ar2faZHE20M/cgRQCpqIiUKFD8BQiaBHWGQdTlGJh7aREsqHsEfBwAt9zbR3h4yZl03lGe/zZqNsX+FpKQO9DQGHER2ern64G2GeaxShOpAgap+xFvj9eHcAG2k3YkgFYGM0o5mmejQ30WdiSv72cvL02sWpZJxsTnECNPKI+hGVe/qR0J4B0+y+AhgBRE0lk/LIpis0rBk3r5p0ru9aVeeC7COSHVk8C7DfrS5LyAVa5ic7bYIfUmlIgkCB7yOikITFICl5wbdvQ62BdIxHJ3AM8rSlp2/tLxCiUxyfWYHvpfiZcS8vbYfiWAG3pvVazN9VvmDjyuhCx5W7uKs20SknFM4TE+qEiEOnO4JmJRppJU+1VUMnk2Qf/TXDn9TFbDUvyeC0TI1RY11i1+8wDwPhgBcsgNwOthhMpcF0BnwV0s5AnnbtS9qNNh9PwX6z25J/dERfI/gMxoAVQrftUAAAAASUVORK5CYII=";
+        private Texture2D componentIcon;
+
+        // Do icon stuff
+        private void OnEnable()
+        {
+            byte[] imageBytes = Convert.FromBase64String(BetterIdentifierComponentIcon);
+            componentIcon = new Texture2D(2, 2);
+            componentIcon.LoadImage(imageBytes);
+        }
+        public override void OnInspectorGUI()
+        {
+            BetterIdentifier component = (BetterIdentifier)target;
+
+            // Gui
+            if (!Application.isPlaying)
+            {
+                GUILayout.Label(new GUIContent("           Modify identifier below or use the button to re-roll its auto generated one!", componentIcon), GUILayout.Height(64));
+            }
+            else
+            {
+                GUILayout.Label(new GUIContent("           This GameObjects ID:", componentIcon), GUILayout.Height(64));
+            }
+
+            EditorGUI.BeginDisabledGroup(Application.isPlaying); // Disable this field if in runtime
+            string newName = EditorGUILayout.TextField(component.Identifier);
+
+            if (GUILayout.Button("Generate Random"))
+            {
+                string randomID = DynamicStorage.NewUUID();
+                component.Identifier = randomID;
+            }
+            else
+            {
+                if (newName != component.Identifier)
+                {
+                    component.Identifier = newName;
+                }
+            }
+
+            EditorGUI.EndDisabledGroup();
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(component);
+            }
+        }
+
+        public override string GetInfoString()
+        {
+            return "Better Identifier";
+        }
+    }
+
+    /// <summary>
+    /// Adds an ID reference list window at Window -> General -> IDify Used IDs
+    /// </summary>
+    public class IdifyCustomWindow : EditorWindow
+    {
+        private Vector2 scrollPosition;
+
+        [MenuItem("Window/General/IDify Used IDs")]
+        public static void ShowWindow()
+        {
+            var window = GetWindow<IdifyCustomWindow>("Idify Used IDs List");
+            window.minSize = new Vector2(100, 600);
+        }
+
+        private void OnGUI()
+        {
+            if (Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("This window does not work during runtime. Return to the editor for it to work.", MessageType.Warning);
+                return;
+            }
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+            foreach (var entry in DynamicStorage.gameObjectIDs)
+            {
+                EditorGUILayout.LabelField(entry.ID);
+            }
+
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
+
+#endif
---
+++ static/apis/5/versions/1/Idify.Runtime.cs
@@ -0,0 +1,284 @@
+// Imports
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+
+/// <summary>
+/// Desc: An API tool for giving GameObjects unique and constant identification, then retrieving them through fast simple lookups.
+/// Usage: Simply add the BetterIdentifier component to GameObjects you would like to register. During runtime, retrieve them by their IDs using Stellar.APIs.ObjectHandlingExtensions.ObjectHandlingExtensions
+/// 
+///                ______
+///            .- '      `-.           
+///          .'            `.         
+///         /                \        
+///        ;                  ;`       
+///        |                  |;
+///        ;                 ;|
+///        '\               / ;       
+///         \`.           .' /        
+///          `.`-._____.- ' .'
+///            / /`_____.- '           
+///           / / /
+///          / / /
+///         / / /
+///        / / /
+///       / / /
+///      / / /
+///     / / /
+///    / / /
+///   / / /
+///   \/_/
+///   
+/// Credit: Written and Documented entirely by BigTylis
+/// </summary>
+namespace Stellar.APIs.Idify.Internal
+{
+    // Better Identifier
+    /// <summary>
+    /// Idify's custom component, used for storing the identifiers and referencing uniqueness. This class cannot be used as is. It is inherited by Stellar.APIs.Idify.BetterIdentifier, so use that for actual use.
+    /// </summary>
+    [ExecuteInEditMode]
+    [DisallowMultipleComponent]
+    [AddComponentMenu("")]
+    public abstract class IdifyAPI : MonoBehaviour
+    {
+        [SerializeField] private string identifier;
+        internal DynamicStorage.GameObjectEntry associatedEntry;
+        public string Identifier
+        {
+            get { return identifier; }
+            set
+            {
+                if (Application.isPlaying || !Application.isEditor) // Only editable in editor
+                {
+                    throw new AccessViolationException("Attempted to edit the ID of a class implementing IIdentifiable but it can only be edited in the Unity Editor.");
+                }
+
+                if (DynamicStorage.AlreadyExists(value))
+                {
+                    Debug.Log("[Idify] Another object already has this ID. Please ensure your ID is unique.");
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    Debug.Log("[Idify] IDs cannot be empty or whitespace.");
+                    return;
+                }
+                if (value.Any(char.IsWhiteSpace))
+                {
+                    Debug.Log("[Idify] IDs cannot contain any whitespace (spaces, tabs, ext.).");
+                    return;
+                }
+
+                identifier = value;
+                associatedEntry.ID = identifier;
+            }
+        }
+
+        [SerializeField] private bool ComponentInitialized = false;
+        private void Reset()
+        {
+            if (!ComponentInitialized)
+            {
+                bool idisunique = false;
+                string newID = string.Empty;
+
+                while (!idisunique)
+                {
+                    newID = DynamicStorage.NewUUID();
+                    idisunique = !DynamicStorage.AlreadyExists(newID);
+                }
+
+                identifier = newID;
+                associatedEntry = new(identifier);
+                DynamicStorage.AddEntry(associatedEntry);
+                ComponentInitialized = true;
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (ComponentInitialized == false)
+                return;
+
+            if (!Application.isPlaying && Application.isEditor)
+            {
+                associatedEntry = new(identifier);
+                DynamicStorage.AddEntry(associatedEntry);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if(associatedEntry != null)
+            {
+                DynamicStorage.RemoveEntry(associatedEntry);
+            }
+        }
+    }
+
+    /// <summary>
+    /// An internal class used for checking the uniqueness of added and edited entries.
+    /// </summary>
+    public static class DynamicStorage
+    {
+        internal class Entry
+        {
+            public string ID;
+            public GameObject GameObject;
+            public IIdentifiable Class;
+            public Type ClassType;
+            public Entry(string id, GameObject gameobject)
+            {
+                ID = id;
+                GameObject = gameobject;
+                ClassType = typeof(GameObject);
+            }
+            public Entry(string id, IIdentifiable any)
+            {
+                ID = id;
+                Class = any;
+                ClassType = any.GetType();
+            }
+        }
+        internal class GameObjectEntry
+        {
+            public string ID;
+            public GameObjectEntry(string id)
+            {
+                ID = id;
+            }
+        }
+
+        internal class EntryGrouping
+        {
+            public ConcurrentBag<Entry> entries = new();
+            public ConcurrentDictionary<string, Entry> entriesLookupManagement = new();
+            public Type ClassType;
+            public EntryGrouping(Type classType)
+            {
+                ClassType = classType;
+            }
+        }
+
+        internal static ConcurrentBag<EntryGrouping> storedGroups = new();
+        internal static HashSet<GameObjectEntry> gameObjectIDs = new();
+
+        /// <summary>
+        /// Gets the group of the specified class type
+        /// </summary>
+        /// <param name="ClassType">Class type</param>
+        /// <returns><see cref="EntryGrouping"/> group</returns>
+        internal static EntryGrouping GetSearchGroup(Type ClassType)
+        {
+            EntryGrouping searchGroup = null;
+            EntryGrouping matchingGroup = storedGroups.FirstOrDefault(group => group.ClassType == ClassType);
+            if (matchingGroup != null)
+            {
+                searchGroup = matchingGroup;
+            }
+
+            if (searchGroup == null)
+            {
+                searchGroup = new EntryGrouping(ClassType);
+                storedGroups.Add(searchGroup);
+            }
+
+            return searchGroup;
+        }
+
+        /// <summary>
+        /// Adds an entry to its group. Entry class types are split into groups to improve lookup speed.
+        /// </summary>
+        /// <param name="entry">New entry</param>
+        internal static void AddEntry(Entry entry)
+        {
+            EntryGrouping grouping = GetSearchGroup(entry.ClassType);
+            if (grouping != null)
+            {
+                grouping.entries.Add(entry);
+                grouping.entriesLookupManagement[entry.ID] = entry;
+            }
+        }
+        /// <summary>
+        /// Adds a <see cref="GameObject"/> ID entry. This system works differently and is less complex than the <see cref="IIdentifiable"/> system.
+        /// </summary>
+        /// <param name="ID"></param>
+        internal static void AddEntry(GameObjectEntry entry)
+        {
+            if (!gameObjectIDs.Contains(entry))
+            {
+                gameObjectIDs.Add(entry);
+            }
+        }
+
+        /// <summary>
+        /// Removes an entry from its group, if it exists.
+        /// </summary>
+        /// <param name="ClassType">Class type for group search</param>
+        /// <param name="ID">String ID</param>
+        internal static void RemoveEntry(Type ClassType, string ID)
+        {
+            EntryGrouping grouping = GetSearchGroup(ClassType);
+
+            if (grouping != null && grouping.entriesLookupManagement.ContainsKey(ID))
+            {
+                if(grouping.entriesLookupManagement.TryGetValue(ID, out var entry))
+                {
+                    List<Entry> temp = grouping.entries.ToList(); temp.Remove(entry);
+                    grouping.entries = new ConcurrentBag<Entry>(temp);
+                    grouping.entriesLookupManagement.TryRemove(ID, out _);
+                }
+            }
+        }
+        /// <summary>
+        /// Removes a <see cref="GameObject"/> ID entry. This system works differently and is less complex than the <see cref="IIdentifiable"/> system.
+        /// </summary>
+        /// <param name="ID"></param>
+        internal static void RemoveEntry(GameObjectEntry entry)
+        {
+            if (gameObjectIDs.Contains(entry))
+            {
+                gameObjectIDs.Remove(entry);
+            }
+        }
+        
+        /// <summary>
+        /// Checks if an ID is already in use anywhere in the game.
+        /// </summary>
+        /// <param name="ID">The ID to search for</param>
+        /// <returns><see cref="bool"/> result</returns>
+        internal static bool AlreadyExists(Type ClassType, string ID)
+        {
+            EntryGrouping grouping = GetSearchGroup(ClassType);
+            if (grouping != null)
+            {
+                return grouping.entriesLookupManagement.ContainsKey(ID);
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Checks if a <see cref="GameObject"/> ID entry exists. This system works differently and is less complex than the <see cref="IIdentifiable"/> system.
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        internal static bool AlreadyExists(string ID)
+        {
+            return gameObjectIDs.FirstOrDefault(e => e.ID == ID) != null;
+        }
+
+        /// <summary>
+        /// Generates a new UUID from System.Guid
+        /// </summary>
+        /// <returns>UUID string</returns>
+        internal static string NewUUID()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
